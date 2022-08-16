26ca74cbdc0dd0e1fff300920e34cbadd77eef15
devninja0722 <legendarysaint0722@gmail.com>
2022-08-16 15:37:37 -0700

    fixed the contrast issues

--- src/pages/Mint.tsx
+++ src/pages/Mint.tsx
@@ -153,6 +153,14 @@ const Mint = () => {
         <div className="flex justify-center font-[400] text-center text-[40px] leading-[52px] lg:text-[50px] md:leading-[65px] pt-[20px] tracking-[3px]">{(isWLed ? wlPrice : pPrice * count).toFixed(2) + " ETH"}</div>
         <div className="flex justify-center font-[400] text-center text-[16px] leading-[21px] lg:text-[14px] md:leading-[18.2px] pt-[20px] tracking-[1px]">(Excluding gas fees)</div>
       </div>
+      <div className="mt-[20px] mb-[40px] md:mt-[30px] grid justify-center">
+        <div className="flex font-[200] max-w-[360px] md:max-w-[450px] text-center text-[#FAFAFA] md:[#FFFFFF] lg:text-[#FFFFFF] text-[10px] leading-[13px] md:text-[14px] md:leading-[16px] pt-[20px] tracking-[0px]">
+          Please make sure you connected to the right network (Ethereum Mainnet) and the correct address. Please note: Once you make the ourchase, you cannot undo this action.
+        </div>
+        <div className="flex font-[200] max-w-[360px] md:max-w-[450px] text-center text-[#FAFAFA] md:[#FFFFFF] lg:text-[#FFFFFF] text-[10px] leading-[13px] md:text-[14px] md:leading-[16px] pt-[20px] tracking-[0px]">
+          We have set the gas limit to 285000 for the contract to successfully mint your NFT. We recommend that you don’t lower the gas limit.
+        </div>
+      </div>
       <div className="flex justify-center items-center z-[100] font-[900] text-[40px] mt-[52px] space-x-[20px] md:space-x-[50px]">
         <CounterButton clickHandler={() => setCount(count > 1 ? count - 1 : count)} buttonCaption="-" disabled={isWLed || !connected} />
         <div className="text-center w-[100px]">{count}</div>
@@ -173,17 +181,10 @@ const Mint = () => {
       <div className={(connected ? "flex" : "hidden").concat(" justify-center mt-[20px]")}>
         <div className="transition-all duration-300 ease-in-out flex justify-center cursor-pointer font-[700] text-center text-[16px] leading-[20.8px] text-[#0094FF] opacity-80 hover:opacity-100 active:opacity-75" onClick={disconnect}>Disconnect</div>
       </div>
-      <div className="mt-[20px] mb-[40px] md:mt-[30px] grid justify-center">
-        <div className="flex font-[200] max-w-[360px] md:max-w-[450px] text-center text-[#FAFAFA] md:[#6D6D6D] lg:text-[#6D6D6D] text-[10px] leading-[13px] md:text-[12px] md:leading-[16px] pt-[20px] tracking-[0px]">
-          Please make sure you connected to the right network (Ethereum Mainnet) and the correct address. Please note: Once you make the ourchase, you cannot undo this action.
-        </div>
-        <div className="flex font-[200] max-w-[360px] md:max-w-[450px] text-center text-[#FAFAFA] md:[#6D6D6D] lg:text-[#6D6D6D] text-[10px] leading-[13px] md:text-[12px] md:leading-[16px] pt-[20px] tracking-[0px]">
-          We have set the gas limit to 285000 for the contract to successfully mint your NFT. We recommend that you don’t lower the gas limit.
-        </div>
-      </div>
       <div className="flex place-content-between items-end w-[calc(100vw)] h-[calc(100vh)] fixed z-[-100] top-0 bg-[url('./assets/img/mint/mintBG.png')] no-repeat bg-right bg-cover md:bg-cover lg:bg-cover">
-        <div className="fixed left-[-160px] md:left-[250px] lg:left-[250px] min-w-[358px] min-h-[435px]"><img src={richard01} alt="" /></div>
-        <div className="fixed right-[-160px] md:right-[250px] lg:right-[250px] min-w-[358px] min-h-[435px]"><img src={richard02} alt="" /></div>
+        <div className="fixed left-[-120px] md:left-[130px] lg:left-[180px] min-w-[358px] min-h-[435px]"><img src={richard01} alt="" /></div>
+        <div className="fixed right-[-120px] md:right-[130px] lg:right-[180px] min-w-[358px] min-h-[435px]"><img src={richard02} alt="" /></div>
+        <div style={{width: "100%", height: "100%", background: "#ffffff2e", zIndex: 1}}></div>
       </div>
       <div className="flex md:hidden lg:hidden fixed z-[-10] w-[calc(100vw)] h-[146px] bottom-0 bg-[url('./assets/img/mint/bottom.png')]"></div>
     </>
